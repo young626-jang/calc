c981d1fd58f60981cac3b26c096fec8b22f4eada
Seoyoung <naraboxer@naver.com>
2025-06-26 10:46:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -215,7 +215,7 @@ export default function App() {
         </table>
 
         <div className="mb-2 text-sm text-gray-700 font-medium">
-          총합: {distributedAmount.toLocaleString()} 원 / 오차: {difference.toLocaleString()} 원
+          총합: {distributedAmount.toLocaleString()}원 / 오차: {difference.toLocaleString()}원
         </div>
 
         <div className="mb-6">
